d1d5ea4fc27e935dd04a2bd2af7ade2360cf5cc9
Julian Cardone <juliancardone1@gmail.com>
2022-09-23 14:51:27 -0400

    completed flight project

--- flight_project/lib/flight.rb
+++ flight_project/lib/flight.rb
@@ -1,3 +1,5 @@
+require_relative "./passenger.rb"
+
 class Flight 
 
     def initialize(flight_number, capacity)
@@ -8,6 +10,27 @@ class Flight
 
     attr_reader :passengers
 
-    
+    def full?
+        return true if @passengers.length == @capacity
+        false
+    end
+
+    def board_passenger(passenger)
+        if !self.full? && passenger.has_flight?(@flight_number)
+            @passengers << passenger
+        end
+    end
+
+    def list_passengers
+        @passengers.map {|inst| inst.name}
+    end
+
+    def [](index)
+        @passengers[index]
+    end
+
+    def <<(passenger)
+        self.board_passenger(passenger)
+    end
 
 end
